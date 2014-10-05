b9d85316411d6d21d4d3e482749634877bf4c6ef
David French <marsstrum@gmail.com>
2014-10-05 16:48:01 +0000

    Public Profiles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,14 +20,14 @@ module ApplicationHelper
 	end
 
 def up_vote_link_classes(post)
-		base = "glyphicon glyphicon-chevron-up"
+		base = "glyphicon glyphicon-chevron-up "
 		vote = current_user.voted(post)
 		base += "voted" if vote && vote.up_vote?
 		base
 	end
 
 	def down_vote_link_classes(post)
-		base = "glyphicon glyphicon-chevron-down"
+		base = "glyphicon glyphicon-chevron-down "
 		vote = current_user.voted(post)
 		base += "voted" if vote && vote.down_vote?
 		base
